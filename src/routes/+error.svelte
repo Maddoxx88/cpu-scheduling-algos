8ac94a89561bb2f4d537fab62e78a7fb68d3e0a6
Sunit Shirke <sunitshirke88@gmail.com>
2025-02-21 00:13:31 -0500

    fix: add 404 route

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,8 @@
 <script>
-    import { page } from '$app/stores';
+    import { page } from '$app/state';
   </script>
   
-  <h1>{$page.status}: {$page?.error.message}</h1>
+  <div class="flex h-screen border justify-center items-center flex-col">
+    <h1 class="text-4xl font-medium text-center">{page.status}: {page.error?.message}</h1>
+    <a href="/" class="text-blue-500 underline my-2">Go home</a>
+  </div>
